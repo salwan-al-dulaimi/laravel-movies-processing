27da84f5c1939ee1a1073e69915f68a35b183024
Salwan-Aldulaimi <salwan@holygrow.nl>
2021-11-17 21:49:08 +0100

    Add profile page

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,45 +1,52 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div>
-                <div class="card-header">{{ __('Profile') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div>
+                    <div class="card-header">{{ __('Profile') }}</div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('user.edit') }}">
-                        @csrf
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('user.edit') }}">
+                            @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-                            <div class="col-md-6">
-                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ $user->name }}</label>
+                            <div class="form-group row">
+                                <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+                                <div class="col-md-6">
+                                    <label for="name"
+                                        class="col-md-4 col-form-label text-md-right">{{ $user->name }}</label>
+                                </div>
                             </div>
-                        </div>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-                            
-                            <div class="col-md-6">
-                                <label for="email" class="col-md-4 col-form-label text-md-right">{{ $user->email }}</label>
-                            </div>
-                        </div>
+                            <div class="form-group row">
+                                <label for="email"
+                                    class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Edit') }}
-                                </button>
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Reset Password') }}
-                                </button>
+                                <div class="col-md-6">
+                                    <label for="email"
+                                        class="col-md-4 col-form-label text-md-right">{{ $user->email }}</label>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Edit') }}
+                                    </button>
+                                    <button type="submit" class="btn btn-primary">
+                                        {{ __('Reset Password') }}
+                                    </button>
+
+                                    <button type="file" class="btn btn-primary btn-floating" multiple>
+                                        <i class="fas fa-upload"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
